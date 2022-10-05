1b705bcd90994058c57833237e6ee0725422f9c9
linhhoang <linhhoang@fortna.com>
2022-10-05 21:26:07 +0700

    dfs with cycled graph

--- src/main/java/Utils.java
+++ src/main/java/Utils.java
@@ -58,4 +58,65 @@ public class Utils {
             node++;
         }
     }
+
+    private static void buildGraphWithColor(int[][] prerequisites, Map<Integer,Integer> degree, Map<Integer, List<Integer>> graph) {
+        int tasks = prerequisites.length;
+        for(int i = 0; i < tasks; i++) {
+            degree.put(i,WHITE);
+            graph.put(i, new ArrayList<>());
+        }
+
+        for(int[] pre : prerequisites) {
+            int source = pre[0];
+            int dest = pre[1];
+            List<Integer> list = graph.get(source);
+            list.add(dest);
+            graph.put(source, list);
+        }
+    }
+
+    final static int WHITE = 0;
+    final static int BLUE = 1;
+    private static List<Integer> findPath(int tasks, int[][] pre) {
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        Map<Integer,Integer> inDegree = new HashMap<>();
+        buildGraphWithColor(pre,inDegree, map);
+        findPathHelper(map,inDegree,0,1, new ArrayList<>());
+        return null;
+    }
+
+    private static void findPathHelper(Map<Integer, List<Integer>> map, Map<Integer,Integer> inDegree, int source, int target, List<Integer> path) {
+        path.add(source);
+        if(source == target) {
+            // found path
+            System.out.println(path);
+            return;
+        }
+
+        if(inDegree.get(source) ==  BLUE) {
+
+        } else {
+            inDegree.put(source,BLUE);
+            List<Integer> children = map.get(source);
+            for(int child : children) {
+                findPathHelper(map,inDegree,child,target,path);
+            }
+        }
+
+        path.remove(path.size() - 1);
+    }
+
+    public static void main(String[] args) {
+        List<Integer> result = Utils.findPath(5,
+                new int[][] { new int[] { 0, 2 }, new int[] { 2, 3 }, new int[] { 3, 4 }, new int[] { 4, 2 }, new int[] { 2, 1 } });
+        System.out.println(result);
+
+//        result = TopologicalSort.sort(5, new int[][] { new int[] { 4, 2 }, new int[] { 4, 3 }, new int[] { 2, 0 },
+//                new int[] { 2, 1 }, new int[] { 3, 1 } });
+//        System.out.println(result);
+//
+//        result = TopologicalSort.sort(7, new int[][] { new int[] { 6, 4 }, new int[] { 6, 2 }, new int[] { 5, 3 },
+//                new int[] { 5, 4 }, new int[] { 3, 0 }, new int[] { 3, 1 }, new int[] { 3, 2 }, new int[] { 4, 1 } });
+//        System.out.println(result);
+    }
 }
--- src/main/java/org/example/_797_AllPathFromSourceToTarget.java
+++ src/main/java/org/example/_797_AllPathFromSourceToTarget.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 public class _797_AllPathFromSourceToTarget {
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
-
+        // TODO done, refactor me
+        return null;
     }
 }
