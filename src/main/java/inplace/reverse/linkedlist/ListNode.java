0c726ad7298d8b25414972b05072725f5f4555ad
linhhoang <linhhoang@fortna.com>
2022-10-15 23:48:08 +0700

    in place reverse list

---
+++ src/main/java/inplace/reverse/linkedlist/ListNode.java
@@ -0,0 +1,10 @@
+package inplace.reverse.linkedlist;
+
+public class ListNode {
+    int value = 0;
+    ListNode next;
+
+    ListNode(int value) {
+        this.value = value;
+    }
+}
---
+++ src/main/java/inplace/reverse/linkedlist/ReverseLinkedList.java
@@ -0,0 +1,34 @@
+package inplace.reverse.linkedlist;
+
+public class ReverseLinkedList {
+    public static ListNode reverse(ListNode head) {
+        // TODO pls recall(encapsulate knowledge)
+        ListNode pre = null;
+        ListNode next = head;
+        while (next != null) {
+            ListNode curNext = next;
+            ListNode tempNext = next.next;
+            curNext.next = pre;
+            pre = next;
+            next = tempNext;
+        }
+
+        return pre;
+    }
+
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(2);
+        head.next = new ListNode(4);
+        head.next.next = new ListNode(6);
+        head.next.next.next = new ListNode(8);
+        head.next.next.next.next = new ListNode(10);
+
+        ListNode result = ReverseLinkedList.reverse(head);
+        System.out.print("Nodes of the reversed LinkedList are: ");
+        while (result != null) {
+            System.out.print(result.value + " ");
+            result = result.next;
+        }
+    }
+}
