30d17f8355fd8eacfcc19dce0570440c33472c14
linhhoang <linhhoang@fortna.com>
2022-10-24 07:35:24 +0700

    max sum

---
+++ src/main/java/slidingWindow/MaxSumSubArrayOfSizeK.java
@@ -0,0 +1,27 @@
+package slidingWindow;
+
+public class MaxSumSubArrayOfSizeK {
+    public static int findMaxSumSubArray(int k, int[] arr) {
+        int maxSum = -1;
+        int windowStart = 0;
+        int sum=0;
+        for(int windowEnd = 0; windowEnd < arr.length; windowEnd++) {
+            sum+=arr[windowEnd];
+            if((windowEnd-windowStart)==k-1) {
+                if(sum>maxSum) {
+                    maxSum=sum;
+                }
+                sum-=arr[windowStart];
+                windowStart++;
+            }
+        }
+        return maxSum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Maximum sum of a subarray of size K: "
+                + MaxSumSubArrayOfSizeK.findMaxSumSubArray(3, new int[] { 2, 1, 5, 1, 3, 2 }));
+        System.out.println("Maximum sum of a subarray of size K: "
+                + MaxSumSubArrayOfSizeK.findMaxSumSubArray(2, new int[] { 2, 3, 4, 1, 5 }));
+    }
+}
