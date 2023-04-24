060073ccde0ab4595f390e75e07c318072edf926
linhhoang <linhhoang@fortna.com>
2023-04-24 21:35:54 +0700

    reverse linked List

---
+++ src/main/java/_75_questions/ReverseLinkedList.java
@@ -0,0 +1,16 @@
+package _75_questions;
+
+public class ReverseLinkedList {
+    public ListNode reverseList(ListNode head) {
+        ListNode previous = null;
+        ListNode current = head;
+
+        while(current != null) {
+            ListNode next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next;
+        }
+        return previous;
+    }
+}
