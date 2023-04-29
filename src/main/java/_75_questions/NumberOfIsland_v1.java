9aa50490a5f5d8a8755458e5f44560e4957d021e
linhhoang <linhhoang@fortna.com>
2023-04-29 09:21:26 +0700

    graph and TODO

---
+++ src/main/java/_75_questions/NumberOfIsland_v1.java
@@ -0,0 +1,34 @@
+package _75_questions;
+
+public class NumberOfIsland_v1 {
+    // TODO enhancement pls
+    public int numIslands(char[][] grid) {
+        System.out.println(grid[0].length);
+        System.out.println(grid.length);
+        int rs = 0;
+        for(int c = 0; c < grid[0].length; c++ ) {
+            for(int r = 0; r < grid.length; r++) {
+                char current = grid[c][r];
+                if(current == '0' || current == 'x') {
+                    continue;
+                } else {
+                    rs++;
+                    numIslandsHelper(grid,c,r);
+                }
+            }
+        }
+        return rs;
+    }
+    public void numIslandsHelper(char[][] grid, int c, int r) {
+        if(c >= grid[0].length || r >= grid.length) {
+            return;
+        }
+        char current = grid[c][r];
+        if(current == '0' || current == 'x') {
+            return;
+        }
+        grid[c][r] = 'x';
+        numIslandsHelper(grid,c+1,r);
+        numIslandsHelper(grid,c,r+1);
+    }
+}
