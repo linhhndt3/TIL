b189ed1f078a001347a96adb89f4b7e37020c59a
linhhoang <linhhoang@fortna.com>
2023-05-15 23:01:17 +0700

    try to understand example

---
+++ src/main/java/_75_questions/GroupAnagram.java
@@ -0,0 +1,11 @@
+package _75_questions;
+
+import java.util.List;
+
+public class GroupAnagram {
+
+    // TODO do not really understanding the problem
+    public List<List<String>> groupAnagrams(String[] strs) {
+        return null;
+    }
+}
