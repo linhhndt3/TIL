63d8ccdee86537c26849659ae8136734b8218bae
linhhoang <linhhoang@fortna.com>
2023-07-12 21:43:10 +0700

    12/07: update hashCode and equals implementation

--- src/main/java/_75_questions/_494_TargetSum.java
+++ src/main/java/_75_questions/_494_TargetSum.java
@@ -52,14 +52,12 @@ class Key {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Key)) return false;
         Key key = (Key) o;
         return index == key.index && target == key.target;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(index, target);
+        return index * 31 + target;
     }
 }
